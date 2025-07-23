d913a62ccf873b3a74250817b6886187900c7996
Aditya Tawde <adityatawde9699@gmail.com>
2025-07-23 21:27:31 +0530

    Delete client/src/components/urls.py

--- client/src/components/urls.py
+++
@@ -1,9 +0,0 @@
-# In your contact/urls.py file
-
-from django.urls import path
-from server.contact.views import ContactView
-
-urlpatterns = [
-    # This URL matches the one in your React component's fetch request
-    path('send/', ContactView.as_view(), name='contact-send'),
-]

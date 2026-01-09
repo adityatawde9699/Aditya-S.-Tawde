a60560a44f7cb28e3e40c4cf8385165504d024b8
Aditya Tawde <adityatawde9699@gmail.com>
2026-01-09 17:00:48 +0530

    api updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
-
+const API_URL = import.meta.env.VITE_API_URL || 
+    (import.meta.env.PROD 
+        ? 'https://aditya-s-tawde-backend.onrender.com/api' 
+        : 'http://127.0.0.1:8000/api');
+        
 const api = axios.create({
     baseURL: API_URL,
     headers: {
